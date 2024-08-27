a30d75b5ae0accdebadffe7d41a6e437facb78c3
esraa-alaa89 <esraaalaa2288@gmail.com>
2024-08-27 17:29:17 +0300

    fix: App

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,21 @@ import Profile from './Components/Profile/Profile';
 import Checkout from './Components/Checkout/Checkout';
 
 let router= createBrowserRouter([
-  {path:'/', element:<Layout/>,  
+  {path:'E-commerce-React_App/', element:<Layout/>,  
     children:[    
-      {path:'/', element:<ProtectedRoute><Home/></ProtectedRoute>},
-      {path:'/home', element:<ProtectedRoute><Home/></ProtectedRoute>},
+      {path:'E-commerce-React_App/', element:<ProtectedRoute><Home/></ProtectedRoute>},
+      {path:'E-commerce-React_App/home', element:<ProtectedRoute><Home/></ProtectedRoute>},
       {path:'/productDetails/:id/:category', element:<ProtectedRoute><ProductDetails/></ProtectedRoute>},
       {path:'/viewAllProducts/:limit/:skip', element:<ProtectedRoute><AllProductsPage/></ProtectedRoute>},
       {path:'/categories/:categoryName', element:<ProtectedRoute><CategoryData/></ProtectedRoute>},
-      {path:'/cart', element:<ProtectedRoute><Cart/></ProtectedRoute>},
-      {path:'/wishlist', element:<ProtectedRoute><WishList/></ProtectedRoute>},
+      {path:'E-commerce-React_App/cart', element:<ProtectedRoute><Cart/></ProtectedRoute>},
+      {path:'E-commerce-React_App/wishlist', element:<ProtectedRoute><WishList/></ProtectedRoute>},
       {path:'/checkout', element:<ProtectedRoute><Checkout/></ProtectedRoute>},
-      {path:'/contact', element:<ProtectedRoute><Contact/></ProtectedRoute>},
-      {path:'/about', element:<ProtectedRoute><About/></ProtectedRoute>},
-      {path:'/profile', element:<ProtectedRoute><Profile/></ProtectedRoute>},
-      {path:'/signup', element:<SignUp/>},
-      {path:'/login', element:<Login/>},
+      {path:'E-commerce-React_App/contact', element:<ProtectedRoute><Contact/></ProtectedRoute>},
+      {path:'E-commerce-React_App/about', element:<ProtectedRoute><About/></ProtectedRoute>},
+      {path:'E-commerce-React_App/profile', element:<ProtectedRoute><Profile/></ProtectedRoute>},
+      {path:'E-commerce-React_App/signup', element:<SignUp/>},
+      {path:'E-commerce-React_App/login', element:<Login/>},
       {path:'*', element:<NotFound/>},
     ]
   }
